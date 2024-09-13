32a042ba74bc3e2aa56d957c7e75c24c85de5fe6
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2024-09-13 23:41:23 +0530

    use chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Aayush-Abhyarthi/test-microservice
 
 go 1.22.4
+
+require github.com/go-chi/chi/v5 v5.1.0
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 
 func handleHome(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello World"))
@@ -8,7 +15,18 @@ func handleHome(w http.ResponseWriter, r *http.Request){
 
 func main(){
 	
-	http.HandleFunc("/", handleHome)
-	http.ListenAndServe(":8000", nil)
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/",handleHome)
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if(err!=nil){
+		fmt.Println("An error occured")
+	}
 
 }
